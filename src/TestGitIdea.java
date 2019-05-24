d29c9dfe6bb830268cc7227019645a5df67fee29
201811281657 <45419597+201811281657@users.noreply.github.com>
2019-05-24 12:20:53 +0800

    Update TestGitIdea.java

--- src/TestGitIdea.java
+++ src/TestGitIdea.java
@@ -3,7 +3,13 @@
  * @date 2019/5/24 12:08
  */
 public class TestGitIdea {
-       pubic String getName(){
-         retrun null;
+       private String name;
+       private int age;
+      public String getName(){
+       return null;
+      }
+       public void setName(String name){
+              this.name=name;
        }
+       
 }
